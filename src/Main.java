fb4d1976e03579f7f34b93d9fe50761ea26867ff
Thisharu-Dilhara <thisharud@gmail.com>
2025-06-26 00:11:49 +0530

    add timmer and TimmerTask

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask(){
+
+            int count = 5;
+
+            @Override
+            public void run(){
+                System.out.println(count);
+                count--;
+
+                if (count < 0){
+                    System.out.println("HAPPY NEW YEAR!");
+
+                }
+
+            }
+        };
+        timer.schedule(task,0);
+
+
+
     }
 }
